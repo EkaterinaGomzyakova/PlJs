3703593a7657c207c346660e656a87d55992cb40
EkaterinaGomzyakova <kate.gm717@gmail.com>
2023-03-28 12:51:04 +0300

    js upd

--- js/script.js
+++ js/script.js
@@ -1,39 +1,31 @@
-// ИЗМЕНЕНИЕ ЦВЕТА
+// ИЗМЕНЕНИЕ ЦВЕТА задать размер div и изображениям на главном экране. для плавного изменения применить для класса opacity: 0 и opacity:1 и в классах show и hide, и прописать Keyframes с transition,  textpath можно переместить в text/ можно через show и hide, но для плавного перехода нужен отдельный класс и keyframes, с transition  и opacity  0 1, через метод css animation play-state: running
 $(document).ready(function(){
+// СКРЫТЬ ЭКРАНЫ КРОМЕ ПЕРВОГО ПРИ ЗАГРУЗКЕ
+  $(".screen_2, .screen_3, .screen_4, .screen_5").hide();
 // ПЕРЕХОД МЕЖДУ ЭКРАНАМИ
   $(".top").click(function(){
-    $(".screen_1, .screen_3, .screen_4, .screen_5").removeClass("show");
-    $(".screen_1, .screen_3, .screen_4, .screen_5").addClass("hide");
-    $(".screen_2").removeClass("hide");
-    $(".screen_2").addClass("show");
+    $(".screen_1, .screen_3, .screen_4, .screen_5").hide();
+    $(".screen_2").show();
   });
 
   $(".right").click(function(){
-    $(".screen_1, .screen_2, .screen_4, .screen_5").removeClass("show");
-    $(".screen_1, .screen_2, .screen_4, .screen_5").addClass("hide");
-    $(".screen_3").removeClass("hide");
-    $(".screen_3").addClass("show");
+    $(".screen_1, .screen_2, .screen_4, .screen_5").hide();
+    $(".screen_3").show();
   });
 
   $(".bottom").click(function(){
-    $(".screen_1, .screen_2, .screen_3, .screen_5").removeClass("show");
-    $(".screen_1, .screen_2, .screen_3, .screen_5").addClass("hide");
-    $(".screen_4").removeClass("hide");
-    $(".screen_4").addClass("show");
+    $(".screen_1, .screen_2, .screen_3, .screen_5").hide();
+    $(".screen_4").show();
   });
 
   $(".left").click(function(){
-    $(".screen_1, .screen_2, .screen_3, .screen_4").removeClass("show");
-    $(".screen_1, .screen_2, .screen_3, .screen_4").addClass("hide");
-    $(".screen_5").removeClass("hide");
-    $(".screen_5").addClass("show");
+    $(".screen_1, .screen_2, .screen_3, .screen_4").hide();
+    $(".screen_5").show();
   });
 
   $(".center").click(function(){
-    $(".screen_2, .screen_3, .screen_4, .screen_5").removeClass("show");
-    $(".screen_2, .screen_3, .screen_4, .screen_5").addClass("hide");
-    $(".screen_1").removeClass("hide");
-    $(".screen_1").addClass("show");
+    $(".screen_2, .screen_3, .screen_4, .screen_5").hide();
+    $(".screen_1").show();
   });
 // ИЗМЕНЕНИЕ ЦВЕТА
   $(".r, .g, .b").on('input', function(){
